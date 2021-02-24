0234138f93be3dc55bac93274642283dc442981f
PresentKim <debe3721@gmail.com>
2021-02-24 16:32:38 +0900

    Init LightSource plugin

---
+++ src/kim/present/lightsource/Loader.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ *  ____                           _   _  ___
+ * |  _ \ _ __ ___  ___  ___ _ __ | |_| |/ (_)_ __ ___
+ * | |_) | '__/ _ \/ __|/ _ \ '_ \| __| ' /| | '_ ` _ \
+ * |  __/| | |  __/\__ \  __/ | | | |_| . \| | | | | | |
+ * |_|   |_|  \___||___/\___|_| |_|\__|_|\_\_|_| |_| |_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author  PresentKim (debe3721@gmail.com)
+ * @link    https://github.com/PresentKim
+ * @license https://www.gnu.org/licenses/lgpl-3.0 LGPL-3.0 License
+ *
+ *   (\ /)
+ *  ( . .) ♥
+ *  c(")(")
+ *
+ * @noinspection PhpIllegalPsrClassPathInspection
+ * @noinspection SpellCheckingInspection
+ */
+
+declare(strict_types=1);
+
+namespace kim\present\lightsource;
+
+use pocketmine\plugin\PluginBase;
+
+class Loader extends PluginBase{
+    public function onEnable() : void{
+    }
+}
